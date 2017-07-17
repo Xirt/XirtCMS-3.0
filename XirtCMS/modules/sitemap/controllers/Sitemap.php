f00a5e61b9fb051557da6f519cc366cd75ceba49
A. Gideonse <agideonse@xirtcms.com>
2017-07-17 22:12:16 +0200

    Corrected template usage

--- XirtCMS/modules/sitemap/controllers/Sitemap.php
+++ XirtCMS/modules/sitemap/controllers/Sitemap.php
@@ -10,71 +10,71 @@
  */
 class Sitemap extends XCMS_Controller {
 
-	/**
+    /**
      * CONSTRUCTOR
      * Instantiates controller with required configuration, helpers, libraries and models
-	 */
-	public function __construct() {
+     */
+    public function __construct() {
 
-		parent::__construct();
+        parent::__construct();
 
-		// Load helpers
-		$this->load->helper("db_search");
-		$this->load->helper("menu");
-		$this->load->helper("url");
+        // Load helpers
+        $this->load->helper("db_search");
+        $this->load->helper("menu");
+        $this->load->helper("url");
 
-		// Load models
-		$this->load->model("MenusModel", "menus");
+        // Load models
+        $this->load->model("MenusModel", "menus");
 
-	}
+    }
 
 
-	/**
+    /**
      * Show sitemap as (X)HTML
-	 */
-	public function index() {
+     */
+    public function index() {
 
-		// Show content
-		XCMS_Page::getInstance()->setTitle("Sitemap", true);
-		$this->load->view("view_html", array(
-			"css_name"   => $this->config("css_name", ""),
+        // Show content
+        XCMS_Page::getInstance()->setTitle("Sitemap", true);
+        $this->load->view("view_html.tpl", array(
+            "css_name"   => $this->config("css_name", ""),
             "show_title" => $this->config("show_title", true),
-			"menus"      => $this->_getContent()
-		));
+            "menus"      => $this->_getContent()
+        ));
 
-	}
+    }
 
 
-	/**
+    /**
      * Show sitemap as XML
-	 */
-	public function xml() {
+     */
+    public function xml() {
 
-		// Prepare items
-		$items = array();
-		foreach ($this->_getContent() as $menu) {
+        // Prepare items
+        $items = array();
+        foreach ($this->_getContent() as $menu) {
 
-			foreach ($menu->items as $item) {
+            foreach ($menu->items as $item) {
 
-				if ($item->type == "module") {
-					$items[] = $item;
-				}
+                if ($item->type == "module") {
+                    $items[] = $item;
+                }
 
-			}
+            }
 
-		}
+        }
 
-		// Disable default template...
-		XCMS_Config::set("USE_TEMPLATE", "FALSE");
+        // Disable default template...
+        XCMS_Config::set("USE_TEMPLATE", "FALSE");
 
-		// ... and show content
-		$this->output->set_content_type("text/xml");
-		$this->load->view("view_xml", array(
-			"baseURL" => base_url(),
-			"items"   => $items
-		));
+        // ... and show content
+        $this->output->set_content_type("text/xml");
+        $this->load->view("view_xml.tpl", array(
+            "baseURL" => base_url(),
+            "items"   => $items
+        ));
 
-	}
+    }
 
 
     /**
@@ -82,43 +82,43 @@ class Sitemap extends XCMS_Controller {
      *
      * @return  array                       Array containing all menus to be displayed
      */
-	private function _getContent() {
+    private function _getContent() {
 
-		$this->menus->load(new SearchAttributes());
-		$menus = $this->menus->toArray();
+        $this->menus->load(new SearchAttributes());
+        $menus = $this->menus->toArray();
 
-		foreach ($menus as $index => $menu) {
+        foreach ($menus as $index => $menu) {
 
-			// Filter hidden menus
-			if (!$menu->sitemap) {
+            // Filter hidden menus
+            if (!$menu->sitemap) {
 
-				unset($menus[$index]);
-				continue;
+                unset($menus[$index]);
+                continue;
 
-			}
+            }
 
-			$hidden = array();
-			$menu->items = array();
+            $hidden = array();
+            $menu->items = array();
 
-			foreach (MenuHelper::getMenu($menu->id, true) as $item) {
+            foreach (MenuHelper::getMenu($menu->id, true) as $item) {
 
-				// Filter hidden nodes
-				if (!$item->sitemap || in_array($item->parent_id, $hidden)) {
+                // Filter hidden nodes
+                if (!$item->sitemap || in_array($item->parent_id, $hidden)) {
 
-					$hidden[] = $item->node_id;
-					continue;
+                    $hidden[] = $item->node_id;
+                    continue;
 
-				}
+                }
 
-				$menu->items[] = $item;
+                $menu->items[] = $item;
 
-			}
+            }
 
-		}
+        }
 
-		return $menus;
+        return $menus;
 
-	}
+    }
 
 }
 ?>
