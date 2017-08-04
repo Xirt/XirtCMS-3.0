c72dc64729d6735b12bf257de4a1fc1fbe4b7843
A. Gideonse <agideonse@xirtcms.com>
2017-08-04 15:18:09 +0200

    Removed trailing spaces (refactor)

--- XirtCMS/models/ArticlesModel.php
+++ XirtCMS/models/ArticlesModel.php
@@ -9,30 +9,30 @@
  * @package     XirtCMS
  */
 class ArticlesModel extends XCMS_Model {
-    
+
     /**
      * Internal list of items
      */
     protected $_list = array();
-    
-    
+
+
     /**
      * CONSTRUCTOR
      * Instantiates controller with required helpers, libraries and models
      */
     public function __construct() {
-        
+
         parent::__construct();
-        
+
         // Load helpers
         $this->load->helper("article");
-        
+
         // Load models
         $this->load->model("ArticleModel", false);
-        
+
     }
-    
-    
+
+
     /**
      * Loads all requested items
      *
@@ -41,17 +41,17 @@ class ArticlesModel extends XCMS_Model {
     public function load() {
 
         if ($this->_loadArticles()) {
-            
+
             $this->_loadAttributes();
             $this->_parseAndFilterArticles();
-            
+
         }
-        
+
         return $this;
 
     }
-    
-    
+
+
     /**
      * Returns the total numbers of rows in the db for the current list (e.g. without limitations)
      *
@@ -60,8 +60,8 @@ class ArticlesModel extends XCMS_Model {
     public function getTotalCount() {
         return $this->_buildArticleQuery()->count_all_results(Query::TABLE_ARTICLES);
     }
-    
-    
+
+
     /**
      * Retrieves the current list of items
      *
@@ -70,81 +70,81 @@ class ArticlesModel extends XCMS_Model {
     public function toArray() {
         return $this->_list;
     }
-    
-    
+
+
     /**
      * Loads all requested items
      *
      * @return  int                         The number of articles loaded
      */
     protected function _loadArticles() {
-        
+
         $query = $this->_buildArticleQuery(func_get_args())->get(Query::TABLE_ARTICLES);
         foreach ($query->result() as $row) {
             $this->_list[$row->id] = (new ArticleModel())->set((array)$row);
         }
-        
+
         return count($this->_list);
-        
+
     }
-    
-    
+
+
     /**
      * Loads attributes for all items currently in the list
      */
     protected function _loadAttributes() {
-        
+
         $query = $this->_buildAttributeQuery(func_get_args())->get(Query::TABLE_ARTICLES_ATTR);
         foreach ($query->result() as $row) {
             $this->_list[$row->ref_id]->setAttribute($row->name, $row->value);
         }
-        
+
     }
-    
-    
+
+
     /**
      * Checks articles and removes invalid ones
      */
     protected function _parseAndFilterArticles() {
-        
-        foreach ($this->_list as $id => $article) {            
-            XCMS_Hooks::execute("articles.parse_article", array($article));            
+
+        foreach ($this->_list as $id => $article) {
+            XCMS_Hooks::execute("articles.parse_article", array($article));
         }
-        
+
     }
-    
-    
+
+
     /**
      * Creates query (using CI QueryBuilder) for retrieving relevant articles
      *
      * @return  Object                      CI Database Instance for chaining purposes
      */
     protected function _buildArticleQuery() {
-        
+
         XCMS_Hooks::execute("articles.build_article_query", array(
             &$this->db)
         );
-        
+
         return $this->db;
-        
+
     }
-    
-    
+
+
     /**
      * Creates query (using CI QueryBuilder) for retrieving relevant attributes
      *
      * @return  Object                      CI Database Instance for chaining purposes
      */
     protected function _buildAttributeQuery() {
-        
+
         $this->db->where_in("ref_id", array_keys($this->_list));
         XCMS_Hooks::execute("articles.build_attribute_query", array(
             &$this->db)
         );
-        
+
         return $this->db;
 
     }
-    
+
 }
 ?>
