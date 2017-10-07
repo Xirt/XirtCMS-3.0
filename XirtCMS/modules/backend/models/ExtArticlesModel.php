ed074c9f14a54bc8d472065f3e37f373fd35b821
A. Gideonse <agideonse@xirtcms.com>
2017-10-07 16:46:21 +0200

    Added updated filtering logic

--- XirtCMS/modules/backend/models/ExtArticlesModel.php
+++ XirtCMS/modules/backend/models/ExtArticlesModel.php
@@ -9,7 +9,13 @@
  * @package     XirtCMS
  */
 class ExtArticlesModel extends ArticlesModel {
-    
+
+    /**
+     * @var array
+     * Attribute array for this model (valid attributes)
+     */
+    protected $_attr = array("filter", "page", "limit", "sortColumn", "sortOrder");
+
     /**
      * Add hooks to influence parent behaviour
      */
@@ -17,16 +23,39 @@ class ExtArticlesModel extends ArticlesModel {
 
         // Hook for article query
         XCMS_Hooks::reset("articles.build_article_query");
-        XCMS_Hooks::add("articles.build_article_query", function($stmt) {
+        XCMS_Hooks::add("articles.build_article_query", function($stmt, $filterOnly) {
 
             // Default query
-            $stmt->select(Query::TABLE_ARTICLES . ".*", Query::TABLE_USERS . ".username")
-                ->join(Query::TABLE_USERS, Query::TABLE_USERS . ".id = author");
-            
+            $stmt->select(Query::TABLE_ARTICLES . ".*, " . Query::TABLE_USERS . ".username AS author")
+                ->join(Query::TABLE_USERS, Query::TABLE_USERS . ".id = author_id");
+
+            // Optional: Filter result
+            if ($filter = trim($this->get("filter"))) {
+
+                $stmt->or_like(array(
+                    Query::TABLE_ARTICLES . ".id"    => $filter,
+                    Query::TABLE_ARTICLES . ".title" => $filter
+                ));
+
+            }
+
+            if (!$filterOnly) {
+
+                 // Optional: Limit amount of rows
+                if (($limit = $this->get("limit")) && is_numeric($limit) && ($page = $this->get("page")) && is_numeric($page)) {
+                    $stmt->limit($limit, ($page - 1) * $limit);
+                }
+
+                // Optional: Sort result
+                $stmt->order_by($this->get("sortColumn"), $this->get("sortOrder"));
+
+            }
+
+
         });
-            
+
         return $this;
-        
+
     }
 
 }
