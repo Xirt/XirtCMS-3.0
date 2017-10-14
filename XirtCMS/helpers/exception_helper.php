0107ac713c3867b6a50ff5d7d39856f7e76bba50
A. Gideonse <agideonse@xirtcms.com>
2017-10-14 17:52:58 +0200

    Helper for custom exceptions

---
+++ XirtCMS/helpers/exception_helper.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Custom exception for use during validations (GUI input checking)
+ *
+ * @author      A.G. Gideonse
+ * @version     3.0
+ * @copyright   XirtCMS 2016 - 2017
+ * @package     XirtCMS
+ */
+class ValidationException extends Exception {
+}
+
+
+/**
+ * Custom exception for use during configuration validation (checking of configuration files)
+ *
+ * @author      A.G. Gideonse
+ * @version     3.0
+ * @copyright   XirtCMS 2016 - 2017
+ * @package     XirtCMS
+ */
+class ConfigurationException extends Exception {
+}
+?>
