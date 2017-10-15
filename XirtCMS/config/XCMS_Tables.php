b06341dc5028360daf7348ecb5a9d87309a5e077
A. Gideonse <agideonse@xirtcms.com>
2017-10-15 16:12:20 +0200

    Initial version

---
+++ XirtCMS/config/XCMS_Tables.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * XirtCMS Table overview for quick reference
+ *
+ * @author      A.G. Gideonse
+ * @version     1.0
+ * @copyright   XirtCMS 2016 - 2017
+ * @package     XirtCMS
+ */
+class XCMS_Tables {
+
+	const TABLE_ARTICLES			 = "xcms_articles";
+	const TABLE_ARTICLES_ATTR		 = "xcms_articles_attributes";
+	const TABLE_ARTICLES_CATEGORIES	 = "xcms_articles_categories";
+	const TABLE_ARTICLES_COMMENTS	 = "xcms_articles_comments";
+	const TABLE_CATEGORIES			 = "xcms_categories";
+	const TABLE_CATEGORIES_RELATIONS = "xcms_categories_relations";
+	const TABLE_MENUITEMS			 = "xcms_menu_items";
+	const TABLE_MENUITEMS_ROUTES	 = "xcms_menu_routes";
+	const TABLE_MENUITEMS_RELATIONS	 = "xcms_menu_relations";
+	const TABLE_CONFIGURATION		 = "xcms_configuration";
+	const TABLE_MENUS				 = "xcms_menus";
+	const TABLE_MODULES				 = "xcms_modules";
+	const TABLE_ROUTES				 = "xcms_routes";
+	const TABLE_MODULES_SETTINGS	 = "xcms_modules_settings";
+	const TABLE_WIDGETS				 = "xcms_widgets";
+	const TABLE_WIDGETS_PAGES		 = "xcms_widgets_pages";
+	const TABLE_WIDGETS_SETTINGS	 = "xcms_widgets_settings";
+	const TABLE_TEMPLATES			 = "xcms_templates";
+	const TABLE_TEMPLATES_POSITIONS	 = "xcms_templates_positions";
+	const TABLE_USERS				 = "xcms_users";
+	const TABLE_USERS_ATTR			 = "xcms_users_attributes";
+	const TABLE_USERGROUPS			 = "xcms_usergroups";
+
+}
+?>
