be2a184499154fca76c798d69ce12f4503d0459a
A. Gideonse <agideonse@xirtcms.com>
2017-10-23 00:09:26 +0200

    Initial version

---
+++ assets/scripts/backend/mng_settings.js
@@ -0,0 +1,126 @@
+$(function() {
+
+	/****************
+	 * PAGE MANAGER *
+	 ****************/
+	$.PageManager = function() {
+	};
+
+	$.PageManager.prototype = {
+
+		init: function() {
+
+			this._initGrid();
+			this._initModals();
+			this._initForms();
+
+			return this;
+
+		},
+
+
+		_initGrid: function() {
+
+			this.grid = (new $.GridManager($("#grid-basic"))).init();
+
+		},
+
+
+		_initModals: function(initializedEditors) {
+
+			modifyModal	= new $.XirtModal($("#modifyModal")).init();
+
+		},
+
+
+		_initForms: function() {
+
+			Form.validate("#form-modify", {
+
+				currentModal: modifyModal,
+				nextModal: modifyModal,
+				grid: this.grid,
+				rules: {
+					config_value: { maxlength: 128 }
+				}
+
+			});
+
+		}
+
+	};
+
+
+	/****************
+	 * GRID MANAGER *
+	 ****************/
+	$.GridManager = function(element) {
+		this.element = (element instanceof $) ? element : $(element);
+	};
+
+	$.GridManager.prototype = {
+
+		init: function() {
+
+			this.element.bootgrid({
+
+				search: true,
+				sorting: true,
+				rowCount: [10, 25, 50, -1],
+				defaultRowCount: +($(window).height() > 1100),
+				ajax: true,
+				url: "backend/settings/view",
+				converters: {
+
+					identifier: {
+						to: function (value) { return Xirt.pad(value, 5, "0"); }
+					}
+
+				},
+				formatters: {
+
+					"commands": function(column, row) {
+						return	"<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-name=\"" + row.name + "\"><span class=\"fa fa-pencil\"></span></button>";
+					}
+
+				}
+
+			}).on("loaded.rs.jquery.bootgrid", $.proxy(this._onload, this));
+
+			return this;
+
+		},
+
+		reload: function() {
+			this.element.bootgrid("reload");
+		},
+
+		_onload: function() {
+
+			this.element.find(".command-edit").on("click", this._modifyContentModal);
+
+		},
+
+		_modifyContentModal: function() {
+
+			modifyModal.load({
+
+				url	: "backend/setting/view/" + $(this).data("name"),
+				onLoad	: function(json) {
+					Xirt.populateForm($("#form-modify"), json, { prefix : "setting_" });
+				}
+
+			});
+
+		}
+
+	};
+
+
+	/***********
+	 * TRIGGER *
+	 **********/
+	var modifyModal;
+	(new $.PageManager()).init();
+
+});
