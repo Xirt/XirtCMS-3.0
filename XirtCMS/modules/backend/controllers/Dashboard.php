0c6e5bdd7622b99912140858922789e6548adab8
A. Gideonse <agideonse@xirtcms.com>
2017-11-12 22:45:52 +0100

    Initial version

---
+++ XirtCMS/modules/backend/controllers/Dashboard.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Controller for the "Dashboard"-GUI and related processes (back end)
+ *
+ * @author      A.G. Gideonse
+ * @version     3.0
+ * @copyright   XirtCMS 2016 - 2017
+ * @package     XirtCMS
+ */
+class Dashboard extends XCMS_Controller {
+
+    /**
+     * CONSTRUCTOR
+     * Instantiates controller with required helpers, libraries and models
+     */
+    public function __construct() {
+
+        parent::__construct(75, true);
+
+    }
+
+
+    /**
+     * Index page for this controller (main GUI)
+     */
+    public function index() {
+
+        $this->load->view("dashboard.tpl");
+
+    }
+
+}
+?>
