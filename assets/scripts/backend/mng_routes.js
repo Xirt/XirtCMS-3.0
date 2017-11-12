a4d1f046129c90d16cb0ecbc7a986eab51cd3527
A. Gideonse <agideonse@xirtcms.com>
2017-11-12 22:43:19 +0100

    Initial version (WIP)

---
+++ assets/scripts/backend/mng_routes.js
@@ -0,0 +1,197 @@
+$(function() {
+
+	/****************
+	 * PAGE MANAGER *
+	 ****************/
+	$.PageManager = function() {
+	};
+
+	$.PageManager.prototype = {
+
+		init: function() {
+
+			this._initGrid();
+			this._initModals();
+			this._initForms();
+			this._initButtons();
+
+			return this;
+
+		},
+
+
+		_initGrid: function() {
+
+			this.grid = (new $.GridManager($("#grid-basic"))).init();
+
+		},
+
+
+		_initForms: function() {
+
+			Form.validate("#form-create", {
+
+				currentModal: createModal,
+				nextModal: createModal,
+				grid: this.grid,
+				rules: {
+					route_public_url: { required: true, maxlength: 256 },
+					route_target_url: { required: true, maxlength: 256 }
+				}
+
+
+			});
+
+			Form.validate("#form-modify", {
+
+				currentModal: modifyModal,
+				nextModal: modifyModal,
+				grid: this.grid,
+				rules: {
+					route_public_url: { required: true, maxlength: 256 },
+					route_target_url: { required: true, maxlength: 256 }
+				}
+
+			});
+
+		},
+
+
+		_initModals: function(initializedEditors) {
+
+			createModal = new $.XirtModal($("#createModal")).init();
+			modifyModal = new $.XirtModal($("#modifyModal")).init();
+
+		},
+
+
+		_initButtons: function() {
+
+			// Activate "Create item"-button
+			$(".btn-create").click(function(e) {
+				createModal.show();
+			});
+
+		}
+
+	};
+
+
+	/****************
+	 * GRID MANAGER *
+	 ****************/
+	$.GridManager = function(element) {
+		this.element = (element instanceof $) ? element : $(element);
+	};
+
+	$.GridManager.prototype = {
+
+		init: function() {
+
+			this.element.bootgrid({
+
+				search: true,
+				sorting: true,
+				rowCount: [10, 20, 50, 100],
+				defaultRowCount: +($(window).height() > 1100),
+				ajax: true,
+				url: "backend/routes/view",
+				converters: {
+
+					identifier: {
+						to: function (value) { return Xirt.pad(value, 5, "0"); }
+					}
+
+				},
+				formatters: {
+
+					"menu_id": function(column, row) {
+						return row.menu_items ? "<i class=\"fa fa-check\"></i>" : "";
+					},
+
+					"commands": function(column, row) {
+
+						return XCMS.createButtons([
+
+							{
+								classNames : "command-edit",
+								data : { id : row.id },
+								icon : "pencil",
+							},
+
+							{
+								additionalAttributes : (row.published == 1) ? "disabled=\"disabled\"" : "",
+								classNames : "command-delete",
+								data : { id : row.id },
+								icon : "trash-o",
+							}
+
+						]);
+
+					}
+
+				}
+
+			}).on("loaded.rs.jquery.bootgrid", $.proxy(this._onload, this));
+
+			return this;
+
+		},
+
+		reload: function() {
+
+			this.element.bootgrid("reload");
+
+		},
+
+		_onload: function() {
+
+			this.element.find(".command-edit").on("click", this._modifyContentModal);
+			this.element.find(".command-delete").on("click", $.proxy(this._deleteItemModal, this));
+
+		},
+
+		_modifyContentModal: function() {
+
+			modifyModal.load({
+
+				url	: "backend/route/view/" + $(this).data("id"),
+				onLoad	: function(json) {
+
+					linkCreator.update("route_", json);
+					Xirt.populateForm($("#form-modify"), json, { prefix : "route_", converters: {
+						id: function (value) { return Xirt.pad(value, 5, "0"); }
+					}});
+
+				}
+
+			});
+
+		},
+
+		_deleteItemModal: function(e) {
+
+			var reference = $(e.currentTarget).data("id");
+			if (jQuery.type(reference) != "undefined") {
+
+				confirmRemoval(
+					"backend/route/remove/" + reference,
+					reference,
+					this
+				);
+
+			}
+
+		}
+
+	};
+
+
+	/***********
+	 * TRIGGER *
+	 **********/
+	var createModal, modifyModal;
+	var linkCreator = (new $.LinkPanel()).init();
+	(new $.PageManager()).init();
+
+});
