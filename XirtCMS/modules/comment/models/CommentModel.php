352b8ad169741d5bc4a747d97afb44661d1356db
A. Gideonse <agideonse@xirtcms.com>
2017-11-22 01:21:34 +0100

    Added method to retrieve comment

--- XirtCMS/modules/comment/models/CommentModel.php
+++ XirtCMS/modules/comment/models/CommentModel.php
@@ -16,8 +16,31 @@ class CommentModel extends XCMS_Model {
      */
     protected $_attr = array(
         "author_id", "author_name", "author_email", "parent_id", "article_id", "content"
-    );    
-    
+    );
+
+
+    /**
+     * Loads the requested comment
+     *
+     * @param   int         $id             The id of the comment to load
+     * @return  mixed                       This instance on success, null otherwise
+     */
+    public function load($id) {
+
+        // Retrieve data from DB
+        $result = $this->db->get_where(XCMS_Tables::TABLE_ARTICLES_COMMENTS, array("id" => $id));
+        if ($result->num_rows()) {
+
+            // Populate model
+            $this->set($result->row());
+            return $this;
+
+        }
+
+        return null;
+
+    }
+
 
     /**
      * Saves the instance in the DB
