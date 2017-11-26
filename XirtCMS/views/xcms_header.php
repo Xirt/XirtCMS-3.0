fdc52baf2aa5dcc68aab4001ca86b10e3e7b2318
A. Gideonse <agideonse@xirtcms.com>
2017-11-26 18:12:59 +0100

    Added default Header template

---
+++ XirtCMS/views/xcms_header.php
@@ -0,0 +1,19 @@
+
+<?php foreach ($metaTags as $name => $content): ?>
+<meta name="<?php echo $name; ?>" content="<?php echo $content; ?>" />
+<?php endforeach; ?>
+
+<title><?php echo $title; ?></title>
+
+<base href="<?php echo $base_url; ?>" />
+<link rel="shortcut icon" href="<?php echo $base_url; ?>assets/images/favicon.ico" type="image/x-icon" />
+
+<!-- CSS / JS Includes [Start]-->
+<?php foreach ($styleSheets as $stylesheet): ?>
+<link type="text/css" href="<?php echo $stylesheet; ?>" rel="stylesheet" />
+<?php endforeach; ?>
+
+<?php foreach ($scripts as $script): ?>
+<script type="text/javascript" src="<?php echo $script; ?>"></script>
+<?php endforeach; ?>
+<!-- CSS / JS Includes [End] -->
